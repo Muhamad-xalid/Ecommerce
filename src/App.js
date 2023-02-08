683395c8c467e1fe8db2bc672aa8f1cd56789b6e
Muhamad-xalid <muhamadxalidahmad00@gmail.com>
2023-02-08 21:07:17 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
-import Navbar from './Components/Navbar';
 import Home from './Pages/Home'
 import About from './Pages/About'
 import Contact from './Pages/Contact'
 import Jewelry from './Pages/Jewelry'
 import Electronics from './Pages/Electronics'
 
-import Mens_clothing from './Pages/Mens_clothing'
-import Womens_clothing from './Pages/Womens_clothing'
+import MensClothing from './Pages/Mens_clothing'
+import WomensClothing from './Pages/Womens_clothing'
 import Product from './Pages/Product';
 import Login from './Pages/Login';
 function App() {
@@ -24,8 +23,8 @@ function App() {
         <Route path="/Contact" element={<Contact />} />
         <Route path="/electronics" element={<Electronics />} />
         <Route path="/jewelery" element={<Jewelry />} />
-        <Route path="/mensclothing" element={<Mens_clothing />} />
-        <Route path="/womensclothing" element={<Womens_clothing />} />
+        <Route path="/mensclothing" element={<MensClothing />} />
+        <Route path="/womensclothing" element={<WomensClothing />} />
         <Route path="/Product" element={<Product />} />
 
       </Routes>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -9,19 +9,19 @@ export default function Home() {
       <ul className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4'>
         <Link to='/electronics' className="bg-gray-900 text-white px-5 py-5 m-3 text-center rounded-lg">
           <h1 className="mb-2 ">electronics</h1>
-          <img className="w-full" src="https://images.pexels.com/photos/777001/pexels-photo-777001.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+          <img className="w-full" alt="electronics img" src="https://images.pexels.com/photos/777001/pexels-photo-777001.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
         </Link>
         <Link to='/jewelery' className="bg-gray-900 text-white px-5 py-5 m-3 text-center rounded-lg">
           <h1 className="mb-2 ">jewelery</h1>
-          <img className="w-full" src="https://images.pexels.com/photos/2697616/pexels-photo-2697616.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>  
+          <img className="w-full" alt="jewelery img" src="https://images.pexels.com/photos/2697616/pexels-photo-2697616.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>  
         </Link>
         <Link to='/mensclothing' className="bg-gray-900 text-white px-5 py-5 m-3 text-center rounded-lg">
           <h1 className="mb-2 ">men's clothing</h1>
-          <img className="w-full" src="https://images.pexels.com/photos/5264909/pexels-photo-5264909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>  
+          <img className="w-full" alt="men's clothing img" src="https://images.pexels.com/photos/5264909/pexels-photo-5264909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>  
         </Link>
         <Link to='/womensclothing' className="bg-gray-900 text-white px-5 py-5 m-3 text-center rounded-lg">
           <h1 className="mb-2 ">women's clothing</h1>
-          <img className="w-full" src="https://images.pexels.com/photos/5864245/pexels-photo-5864245.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+          <img className="w-full" alt="women's clothing img" src="https://images.pexels.com/photos/5864245/pexels-photo-5864245.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
         </Link>
       </ul>
      </div>
